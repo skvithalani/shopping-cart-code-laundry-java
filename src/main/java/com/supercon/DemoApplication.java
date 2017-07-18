74df8699f0da836d75d20d3c27616aab9b9bea9e
Will James <wjames@thoughtworks.com>
2017-07-18 11:34:34 +1000

    Revert "Moved source to snrdev-java subdirectory of Git repo."
    This reverts commit 25b64a2f69042164fe039fb02fddb8794dff86d6.

--- src/main/java/com/supercon/DemoApplication.java
+++ src/main/java/com/supercon/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductsApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductsApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 }
