8276cdf0381ea664407dcef2242a3f210babd142
Michael Strasser <mstrass@thoughtworks.com>
2017-08-01 15:54:42 +1000

    Office furniture products; removed application test

--- src/main/java/com/supercon/service/ProductService.java
+++ src/main/java/com/supercon/service/ProductService.java
@@ -10,10 +10,17 @@ import java.util.stream.Collectors;
 @Service
 public class ProductService {
 
+    // Simple source of products, good enough for now.
     private static final List<Product> ALL_PRODUCTS = Arrays.asList(
-            new Product(9.99, "PROD_01", "Ekin T-shirt"),
-            new Product(15.99, "DIS_10-PROD_02", "Sadida running shoes"),
-            new Product(14.50, "DIS_15-PROD_03", "Sadida T-shirt")
+            new Product(24.99, "CHAIR_RED", "Red plastic chair"),
+            new Product(24.99, "DIS_10-CHAIR_BLUE", "Blue plastic chair"),
+            new Product(24.99, "CHAIR_WHITE", "White plastic chair"),
+            new Product(14.99, "STOOL_WHITE", "White plastic footstool"),
+            new Product(14.99, "DIS_15-STOOL_GREEN", "Green plastic footstool"),
+            new Product(74.99, "COMP_DESK", "Wooden computer desk"),
+            new Product(129.99, "COMP_CHAIR", "Computer swivel chair"),
+            new Product(249.99, "BOARD_TABLE", "12-person boardroom table"),
+            new Product(99.99, "BOARD_CHAIR", "Boardroom chair")
     );
 
     private List<Product> products;
